e0a8af2a372d7d159dc4423f15095980e1965e95
Lee C <lcurtis9000@gmail.com>
2019-03-22 14:07:24 -0400

    only missing the bit where the dots reflect the chord'

--- src/ChordDiagram.js
+++ src/ChordDiagram.js
@@ -17,6 +17,8 @@ class ChordDiagram extends Component {
         note6fret: 1
     };
 
+    notesInOrder = ['Ab','A','Bb','B','C','Db','D','Eb','E','F','Gb','G','Ab','A','Bb','B','C','Db','D','Eb','E','F','Gb','G'];
+    
     getCircleX = (note) => {
         var fret = this.state['note'+ note +'fret'];
         if (fret < 0)
@@ -31,25 +33,88 @@ class ChordDiagram extends Component {
         return 28*string;
     };
 
+    determineNotesInChord = () => {
+        //figure out the letter notes that belong in this chord
+        const { rootNote } = this.props;
+        const { chordType } = this.props;
+
+        var rootIndex = this.notesInOrder.indexOf(rootNote);
+        var noteLetters = [rootNote];
+
+        if (chordType==='maj')
+        {
+            noteLetters.push(this.notesInOrder[rootIndex+4]); //3rd
+            noteLetters.push(this.notesInOrder[rootIndex+7]); //5th
+        }
+        else if (chordType==='min')
+        {
+            noteLetters.push(this.notesInOrder[rootIndex+3]);
+            noteLetters.push(this.notesInOrder[rootIndex+7]);
+        }
+        else if (chordType==='dim')
+        {
+            noteLetters.push(this.notesInOrder[rootIndex+3]);
+            noteLetters.push(this.notesInOrder[rootIndex+6]);
+        }
+        else if (chordType==='+')
+        {
+            noteLetters.push(this.notesInOrder[rootIndex+5]);
+            noteLetters.push(this.notesInOrder[rootIndex+8]);
+        }
+        else if (chordType==='min')
+        {
+            noteLetters.push(this.notesInOrder[rootIndex+3]);
+            noteLetters.push(this.notesInOrder[rootIndex+7]);
+        }
+        else if (chordType==='7')
+        {
+            noteLetters.push(this.notesInOrder[rootIndex+4]);
+            noteLetters.push(this.notesInOrder[rootIndex+7]);
+            noteLetters.push(this.notesInOrder[rootIndex+10]);
+        }
+        else if (chordType==='7')
+        {
+            noteLetters.push(this.notesInOrder[rootIndex+4]);
+            noteLetters.push(this.notesInOrder[rootIndex+7]);
+            noteLetters.push(this.notesInOrder[rootIndex+10]);
+        }
+        else if (chordType==='maj7')
+        {
+            noteLetters.push(this.notesInOrder[rootIndex+4]);
+            noteLetters.push(this.notesInOrder[rootIndex+7]);
+            noteLetters.push(this.notesInOrder[rootIndex+11]);
+        }
+        else if (chordType==='m7')
+        {
+            noteLetters.push(this.notesInOrder[rootIndex+3]);
+            noteLetters.push(this.notesInOrder[rootIndex+7]);
+            noteLetters.push(this.notesInOrder[rootIndex+10]);
+        }
+
+        console.log(noteLetters);
+    };
+
     render() {
+        this.determineNotesInChord();
         return (
+
         <div>
             <svg height="200" width="820">
                 <rect x="0" y="14" width="820" height="170" fill="peru"/>
                 <rect x="20" y="14" width="10" height="170" fill="papayawhip"/>
 
-                <line x1="94" y1="14" x2="94" y2="184" stroke="silver" stroke-width="2"/>
-                <line x1="158" y1="14" x2="158" y2="184" stroke="silver" stroke-width="2"/>
-                <line x1="222" y1="14" x2="222" y2="184" stroke="silver" stroke-width="2"/>
-                <line x1="286" y1="14" x2="286" y2="184" stroke="silver" stroke-width="2"/>
-                <line x1="350" y1="14" x2="350" y2="184" stroke="silver" stroke-width="2"/>
-                <line x1="414" y1="14" x2="414" y2="184" stroke="silver" stroke-width="2"/>
-                <line x1="478" y1="14" x2="478" y2="184" stroke="silver" stroke-width="2"/>
-                <line x1="542" y1="14" x2="542" y2="184" stroke="silver" stroke-width="2"/>
-                <line x1="606" y1="14" x2="606" y2="184" stroke="silver" stroke-width="2"/>
-                <line x1="670" y1="14" x2="670" y2="184" stroke="silver" stroke-width="2"/>
-                <line x1="734" y1="14" x2="734" y2="184" stroke="silver" stroke-width="2"/>
-                <line x1="798" y1="14" x2="798" y2="184" stroke="silver" stroke-width="2"/>
+                <line x1="94" y1="14" x2="94" y2="184" stroke="silver" strokeWidth="2"/>
+                <line x1="158" y1="14" x2="158" y2="184" stroke="silver" strokeWidth="2"/>
+                <line x1="222" y1="14" x2="222" y2="184" stroke="silver" strokeWidth="2"/>
+                <line x1="286" y1="14" x2="286" y2="184" stroke="silver" strokeWidth="2"/>
+                <line x1="350" y1="14" x2="350" y2="184" stroke="silver" strokeWidth="2"/>
+                <line x1="414" y1="14" x2="414" y2="184" stroke="silver" strokeWidth="2"/>
+                <line x1="478" y1="14" x2="478" y2="184" stroke="silver" strokeWidth="2"/>
+                <line x1="542" y1="14" x2="542" y2="184" stroke="silver" strokeWidth="2"/>
+                <line x1="606" y1="14" x2="606" y2="184" stroke="silver" strokeWidth="2"/>
+                <line x1="670" y1="14" x2="670" y2="184" stroke="silver" strokeWidth="2"/>
+                <line x1="734" y1="14" x2="734" y2="184" stroke="silver" strokeWidth="2"/>
+                <line x1="798" y1="14" x2="798" y2="184" stroke="silver" strokeWidth="2"/>
 
                 <line x1="0" y1="28" x2="820" y2="28" stroke="black" />
                 <line x1="0" y1="56" x2="820" y2="56" stroke="black" />
--- src/ChordSelector.js
+++ src/ChordSelector.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Dropdown} from 'primereact/dropdown';
+import {Button} from 'primereact/button';
 import 'primereact/resources/themes/luna-green/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
@@ -11,68 +11,55 @@ class ChordSelector extends Component {
   state = {
       rootNote: 'F',
       chordType: 'maj',
-      chordAdditions: 'None'
   };
-
-  notesInOrder = ['Ab','A','Bb','B','C','Db','D','Eb','E','F','Gb','G','Ab','A','Bb','B','C','Db','D','Eb','E','F','Gb','G'];
-
-  chordAdditions = [
-      {label: 'None', value:'None'},
-      {label: '7', value: '7'},
-      {label: '9', value: '9'},
-      {label: 'b5', value: 'b5'}
-  ];
   
   chordTypeOptions = [
       {label: 'Major', value:'maj'},
       {label: 'Minor', value:'min'},
       {label: 'Dim', value:'Dim'},
       {label: 'Aug', value:'Aug'},
-  ];
-  
-  rootNoteOptions =  [
-      {label: 'Ab', value: 'Ab'},
-      {label: 'A', value: 'A'},
-      {label: 'Bb', value: 'Bb'},
-      {label: 'B', value: 'B'},
-      {label: 'C', value: 'C'},
-      {label: 'Db', value: 'Db'},
-      {label: 'D', value: 'D'},
-      {label: 'Eb', value: 'Eb'},
-      {label: 'E', value: 'E'},
-      {label: 'F', value: 'F'},
-      {label: 'Gb', value: 'Gb'},
-      {label: 'G', value: 'G'},
-  ];
-  
+  ];  
 
-    handleRootSelection = (event) => {
-      this.setState({rootNote: event.value});
+    handleRootSelection = (value) => {
+      this.setState(state=>({rootNote: value}));
     };
 
-    handleTypeSelection = (event) => {
-      this.setState({chordType: event.value});
-    };
-
-    handleAddSelection = (event) => {
-      this.setState({chordAdditions: event.value});
-    };
-
+    handleTypeSelection = (value) => {
+      this.setState(state=>({chordType: value}));
+    };   
 
     render() {
       return (
         <div>
           <p style={{margin: '5px'}}>Selected Chord: {this.state.rootNote + 
-                                      (this.state.chordType !== 'maj' ? this.state.chordType : '') + 
-                                      (this.state.chordAdditions !== 'None' ? this.state.chordAdditions : '')}
+                                      (this.state.chordType !== 'maj' ? this.state.chordType : '')}
           </p>
           <div  align="left">
-            <Dropdown id='rootNote' style={{margin: '5px'}} value={this.state.rootNote} options={this.rootNoteOptions} onChange={this.handleRootSelection}/>
-            <Dropdown id='chordType' style={{margin: '5px'}} value={this.state.chordType} options={this.chordTypeOptions} onChange={this.handleTypeSelection}/>
-            <Dropdown id='chordType' style={{margin: '5px'}} value={this.state.chordAdditions} options={this.chordAdditions} onChange={this.handleAddSelection}/>
+            <Button  id='Ab' label='Ab' onClick={()=>this.handleRootSelection('Ab')} style={{margin: '2px'}}/>
+            <Button id='A' label='A' onClick={()=>this.handleRootSelection('A')} style={{margin: '2px'}}/>
+            <Button id='Bb' label='Bb' onClick={()=>this.handleRootSelection('Bb')} style={{margin: '2px'}}/>
+            <Button id='B' label='B' onClick={()=>this.handleRootSelection('B')} style={{margin: '2px'}}/>
+            <Button id='C' label='C' onClick={()=>this.handleRootSelection('C')} style={{margin: '2px'}}/>
+            <Button id='Db' label='Db' onClick={()=>this.handleRootSelection('Db')} style={{margin: '2px'}}/>
+            <Button id='D' label='D' onClick={()=>this.handleRootSelection('D')} style={{margin: '2px'}}/>
+            <Button id='Eb' label='Eb' onClick={()=>this.handleRootSelection('Eb')} style={{margin: '2px'}}/>
+            <Button id='E' label='E' onClick={()=>this.handleRootSelection('E')} style={{margin: '2px'}}/>
+            <Button id='F' label='F' onClick={()=>this.handleRootSelection('F')} style={{margin: '2px'}}/>
+            <Button id='Gb' label='Gb' onClick={()=>this.handleRootSelection('Gb')} style={{margin: '2px'}}/>
+            <Button id='G' label='G' onClick={()=>this.handleRootSelection('G')} style={{margin: '2px'}}/>
+          </div>
+          <hr/>
+          <div  align="left">
+            <Button id='Major' label={this.state.rootNote+' Major'} onClick={()=>this.handleTypeSelection('maj')} style={{margin: '2px'}}/>
+            <Button id='Minor' label={this.state.rootNote+' Minor'} onClick={()=>this.handleTypeSelection('min')} style={{margin: '2px'}}/>
+            <Button id='Diminished' label={this.state.rootNote+' Diminished'} onClick={()=>this.handleTypeSelection('dim')} style={{margin: '2px'}}/>
+            <Button id='Augmented' label={this.state.rootNote+' Augmented'} onClick={()=>this.handleTypeSelection('+')} style={{margin: '2px'}}/>
+            <Button id='7' label={this.state.rootNote+'7'} onClick={()=>this.handleTypeSelection('7')} style={{margin: '2px'}}/>
+            <Button id='maj7' label={this.state.rootNote+'maj7'} onClick={()=>this.handleTypeSelection('maj7')} style={{margin: '2px'}}/>
+            <Button id='m7' label={this.state.rootNote+'m7'} onClick={()=>this.handleTypeSelection('m7')} style={{margin: '2px'}}/>
           </div>
           <br/>
-          <ChordDiagram rootNote={this.state.rootNote} chordType={this.state.chordType} additions={this.state.chordAdditions}/>
+          <ChordDiagram rootNote={this.state.rootNote} chordType={this.state.chordType}/>
         </div>
       );
     }
